ef2cc46363232cba3b5466806db4acf969b3ff29
chris.mcqueen <chris.mcqueen@capgemini.com>
2022-04-12 15:07:48 +0200

    Add feedback stats

--- src/App.js
+++ src/App.js
@@ -4,14 +4,22 @@ import Header from './components/Header';
 import { useState } from 'react';
 import FeedbackData from './data/FeedbackData';
 import FeedbackList from './components/FeedbackList';
+import FeedbackStats from './components/FeedbackStats';
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
+  const deleteFeedback = (id) => {
+    if(window.confirm('Are you sure you want to delete?')) {
+      /* delete all the items in the feedback array that aren't equal to the selected id */
+      setFeedback(feedback.filter((item)=> item.id !==id)
+   )}
+  };
   return (
     <>
       <Header />
       <div className='container'>
-        <FeedbackList feedback={feedback} />
+        <FeedbackStats feedback= {feedback} />
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </>
   );
--- src/components/FeedbackItem.js
+++ src/components/FeedbackItem.js
@@ -1,11 +1,20 @@
 import React from 'react';
-const FeedbackItem = ({ item }) => {
+import { FaTimes } from 'react-icons/fa';
+import Card from './shared/Card';
+import PropTypes from 'prop-types';
+const FeedbackItem = ({ item, handleDelete }) => {
   return (
-    <div className='card'>
+    <Card reverse={true}>
       <div className='num-display'>{item.rating}</div>
+      <button onClick={() => handleDelete(item.id)} className='close'>
+        <FaTimes color='purple' />
+      </button>
       <div className='text-display'>{item.text}</div>
-    </div>
+    </Card>
   );
 };
 
+FeedbackItem.propTypes = {
+  item: PropTypes.object.isRequired,
+};
 export default FeedbackItem;
