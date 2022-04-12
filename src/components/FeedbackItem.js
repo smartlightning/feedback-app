d6bea004db0fc142f87d06a639e21c2d0ea23be9
chris.mcqueen <chris.mcqueen@capgemini.com>
2022-04-12 10:21:22 +0200

    Add card and header

---
+++ src/components/FeedbackItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FeedbackItem = () => {
+  return (
+    <div className='card'>
+      <div className='num-display'>10</div>
+      <div className='text-display'>Example of feedback item</div>
+    </div>
+  );
+};
+
+export default FeedbackItem;
